62df55d06a1bf6d85cac6904370b49dff3126a5d
Gabriel Carvalho <117952203+GabrielCP0@users.noreply.github.com>
2022-11-25 18:50:13 -0300

    Tipos de dados nativos no python.

---
+++ Tipos_de_dados.py
@@ -0,0 +1,40 @@
+# Inteiro (int)
+inteiro = 1
+print(type(inteiro))
+
+
+# Ponto Flutuante ou Decimal (float)
+decimal = 1.2
+print(type(decimal))
+
+
+
+#String (str)
+string = "Olá mundo"
+print(type(string))
+
+
+# Boolean (bool)
+#True ou False
+booleano = True
+print(type(booleano))
+
+
+# List (list)
+lista = [1,2,3,4,5]
+print(type(lista))
+
+
+# Tuple
+tupla = (1,2,3,4,5)
+print(type(tupla))
+
+# Dictionary (dic)
+dicionario = {'linguagem': 'Python'}
+print(type(dicionario))
+
+
+# Tipo Complexo (complex) Geralmente usado em cálculos 
+#geométricos ou matemáticos
+complexo = 5+2j
+print(type(complexo))
